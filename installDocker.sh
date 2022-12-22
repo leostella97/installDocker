d3e5b714d0944239dc033ecfce4a6f25f020de39
Leonardo Stella <leonstella97@gmail.com>
2022-12-22 13:31:12 -0300

    Inserção do script ao repositório

---
+++ installDocker.sh
@@ -0,0 +1,13 @@
+##iniciar script bash
+#!/bin/bash
+
+## atualizar sistema
+apt update -y
+apt upgrade -y
+
+##baixa o script do docker oficial e instala
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh -y
+
+##mostra a versão, teste para ver se instalou com sucesso
+docker version
